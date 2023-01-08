26acfa17145c706acdaa0bb12a225c9aaf9fc23f
Danseren <104249031+Danseren@users.noreply.github.com>
2023-01-08 11:57:57 +0500

    Урок 5 | Добавил OkHttp, Retrofit, Glide, разрешения в манифест (INTERNET и ACCESS_NETWORK_STATE) и buildConfigField с адресом GitHub API

--- app/src/main/java/ru/sample/store/myapplication/core/navigation/IScreens.kt
+++ app/src/main/java/ru/sample/store/myapplication/core/navigation/IScreens.kt
@@ -1,6 +1,5 @@
 package ru.sample.store.myapplication.core.navigation
 
-import android.os.Bundle
 import com.github.terrakok.cicerone.Screen
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 import ru.sample.store.myapplication.model.GithubUser
